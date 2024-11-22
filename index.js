b92c4143004109595fdca5dce44823aa89a3facf
ebokoo <softonus111@gmail.com>
2024-11-23 02:27:48 +0800

    v1.1.1

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { parsers: typescriptParsers } = require('prettier/parser-typescript')
 const { parsers: javascriptParsers } = require('prettier/parser-babel')
+const { parsers: htmlParsers } = require('prettier/parser-html')
 
 function preprocess(text, opts) {
   const classNamePattern = /className\s*=\s*["']([^"']+)["']/g
   return text.replace(classNamePattern, (match, classNames) => {
     const cleanClassNames = classNames.trim().replace(/\s+/g, ' ')
     return `className="${cleanClassNames}"`
+  })
+}
+
+function preprocessVue(text, opts) {
+  const classNamePatternVue = /class\s*=\s*["']([^"']+)["']/g
+  return text.replace(classNamePatternVue, (match, classNames) => {
+    const cleanClassNames = classNames.trim().replace(/\s+/g, ' ')
+    return `class="${cleanClassNames}"`
   })
 }
 
@@ -18,6 +27,10 @@ module.exports = {
     babel: {
       ...javascriptParsers.babel,
       preprocess
+    },
+    vue: {
+      ...htmlParsers.html,
+      preprocess: preprocessVue
     }
   }
 }
